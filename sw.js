526d304632a78e58bc4718254fcfad63e08a74f7
Ndagijimana Sebastien <sebastian.ndagijimana@ist.com>
2026-06-22 11:30:52 +0200

    Initial commit: Bullet Journal PWA with Google Sheets + Tasks sync

---
+++ sw.js
@@ -0,0 +1,21 @@
+const CACHE = 'bujo-v2';
+const ASSETS = ['/', '/index.html', '/manifest.json', '/icons/icon-192.png', '/icons/icon-512.png'];
+
+self.addEventListener('install', ev => {
+  ev.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
+});
+
+self.addEventListener('activate', ev => {
+  ev.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim()));
+});
+
+self.addEventListener('fetch', ev => {
+  if (ev.request.method !== 'GET') return;
+  ev.respondWith(
+    caches.match(ev.request).then(cached => cached || fetch(ev.request).then(res => {
+      const clone = res.clone();
+      caches.open(CACHE).then(c => c.put(ev.request, clone));
+      return res;
+    }))
+  );
+});
